5514ed67ae1f4d453f25e8df7b81284dd3bf7993
alix103 <atbermudez96@misena.edu.co>
2022-11-29 15:45:03 -0500

    first commit

---
+++ js-inicio/inicio.js
@@ -0,0 +1,23 @@
+//Declarar Variables
+
+//Variable tipo char
+var carName = "Volvo";
+
+//Variable tipo number
+var x = 50;
+
+//Suma de dos números
+var x = 5;
+var y = 10;
+
+document.getElementById("demo").innerHTML = x + y;
+
+//Suma de dos números llamando una variable z
+var x = 5;
+var y = 10;
+var z = x + y;
+
+alert(z);
+
+//Declarar variables en una sola línea
+var firstName = "John", lastName = "Doe", age = 35;
