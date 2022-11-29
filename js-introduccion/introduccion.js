485e79d2fb9086654aaf2c1f75374b6f59604066
alix103 <atbermudez96@misena.edu.co>
2022-11-29 18:21:17 -0500

    Avances en JS

---
+++ js-introduccion/introduccion.js
@@ -0,0 +1,9 @@
+//Imprimir datos con getElementById()
+
+//Con comillas dobles ("")
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+//Con comillas simples ('')
+document.getElementById('demo1').innerHTML = 'Hello JavaScript';
+
+// 
